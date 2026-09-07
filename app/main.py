d60e1d9a9c3839764c53c26ec6d9ee54f8fa2e76
obinmahfoudh <binmahfoudhobaid@gmail.com>
2026-09-07 17:53:17 -0500

    Planning to host on render so changed startup to allow render to set port and still allow to locally host

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.ingestion_routes import router as ingest_router
 from app.api.query_routes import router as query_router
+import uvicorn
 
 app = FastAPI(
     title="Enterprise RAG Search Engine",
@@ -17,9 +19,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(ingest_router, prefix="api/")
-app.include_router(query_router, prefix="api/")
+app.include_router(ingest_router, prefix="/api")
+app.include_router(query_router, prefix="/api")
 
 @app.get("/healthz")
 async def health_check():
     return {"status": "healthy", "service": "rag-backend"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:main:app", host="0.0.0.0", port=port, reload=False)
